1e8e0ad9af713a82697a84f111651ffe760f7419
Malindu <malindu@missamara.com.au>
2026-01-21 21:04:31 +0530

    SSO fix

--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -27,8 +27,9 @@ function Sidebar() {
   const handleSignOut = async () => {
     setLoading(true);
     try {
-      await signOut({ global: true });
-      window.location.reload();
+      await signOut({global: true});
+    } catch (err) {
+      console.error("Logout failed", err);
     } finally {
       setLoading(false);
     }
